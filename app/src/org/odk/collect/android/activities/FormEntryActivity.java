87d45c75425930081ba39237d59faed1cedc54af
Phillip Mates <pmates@dimagi.com>
2015-08-31 22:13:58 -0400

    Generic annotations and unnecessary casts

--- app/src/org/odk/collect/android/activities/FormEntryActivity.java
+++ app/src/org/odk/collect/android/activities/FormEntryActivity.java
@@ -956,8 +956,8 @@ public class FormEntryActivity extends FragmentActivity implements AnimationList
 
     private void updateFloatingLabels(View currentView) {
         //TODO: this should actually be set up to scale per screen size.
-        ArrayList<Pair<String, FloatingLabel>> smallLabels = new ArrayList<Pair<String, FloatingLabel>>();
-        ArrayList<Pair<String, FloatingLabel>> largeLabels = new ArrayList<Pair<String, FloatingLabel>>();
+        ArrayList<Pair<String, FloatingLabel>> smallLabels = new ArrayList<>();
+        ArrayList<Pair<String, FloatingLabel>> largeLabels = new ArrayList<>();
         
         FloatingLabel[] labelTypes = FloatingLabel.values();
         
@@ -969,9 +969,9 @@ public class FormEntryActivity extends FragmentActivity implements AnimationList
                     if(type.getAppearance().equals(hint)) {
                         String widgetText = widget.getPrompt().getQuestionText();
                         if(widgetText != null && widgetText.length() < 15) {
-                            smallLabels.add(new Pair(widgetText, type));
+                            smallLabels.add(new Pair<>(widgetText, type));
                         } else {
-                            largeLabels.add(new Pair(widgetText, type));
+                            largeLabels.add(new Pair<>(widgetText, type));
                         }
                     }
                 }
@@ -2854,7 +2854,7 @@ public class FormEntryActivity extends FragmentActivity implements AnimationList
             if (fragmentClass != null) {
                 final FragmentManager fm = this.getSupportFragmentManager();
 
-                Fragment bar = (Fragment) fm.findFragmentByTag(TITLE_FRAGMENT_TAG);
+                Fragment bar = fm.findFragmentByTag(TITLE_FRAGMENT_TAG);
                 if (bar == null) {
                     try {
                         bar = ((Class<Fragment>)Class.forName(fragmentClass)).newInstance();
